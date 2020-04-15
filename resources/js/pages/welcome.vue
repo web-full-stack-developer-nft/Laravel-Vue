696ec249699bdea7a6e56558cd1ce38dcbb0c5da
web-full-stack-developer-nft <jd3606192@gmail.com>
2020-04-15 21:55:46 +0600

    make ui cart

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-	<div class="flex">
-		<div class="bg-white shadow-lg w-1/4 p-5 border-l-4 rounded-md border-green-500 m-2 flex items-center justify-between">
+	<div class="flex flex-wrap md:flex-wrap lg:flex-wrap justify-center">
+		<div class="bg-white shadow-lg w-full md:w-1/3 lg:w-1/3 flex-grow xl:w-1/5 p-5 border-l-4 rounded-md border-green-500 m-2 flex items-center justify-between">
 			<div>
 				<h5 class="uppercase text-xs text-green-500">Users</h5>
 				<h3 class="text-xl font-bold">7918</h3>
@@ -10,7 +10,7 @@
 				<svg aria-hidden="true" class="h-8 w-8 text-gray-800" data-prefix="fad" data-icon="dollar-sign" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"><g class="fa-group"><path fill="currentColor" d="M176 64h-64V16a16 16 0 0116-16h32a16 16 0 0116 16zm-64 384v48a16 16 0 0016 16h32a16 16 0 0016-16v-48z" class="fa-secondary"/><path fill="currentColor" d="M281.72 375.1c-15.4 44.1-59.11 71.8-105.72 72.7l-64 .2a141.39 141.39 0 01-86.51-30c-7.9-6.1-8.9-17.6-1.8-24.5l34.81-34a15.6 15.6 0 0119.5-2 61 61 0 0034.19 10.5h66.31a29.49 29.49 0 008.3-57.8l-102.52-30c-44.4-13-79.61-50.5-83.81-96.6A109.57 109.57 0 0199.55 64.46c3.3-.3 6.61-.46 9.92-.46H176a141.44 141.44 0 0186.52 30c7.9 6.1 8.9 17.6 1.8 24.5l-34.81 34a15.6 15.6 0 01-19.5 2A61.1 61.1 0 00175.8 144h-66.31a29.49 29.49 0 00-8.3 57.8l108 31.6c57.93 16.9 94.04 80.1 72.53 141.7z" class="fa-primary"/></g></svg>
 			</div>
 		</div>
-		<div class="bg-white shadow-lg w-1/4 p-5 border-l-4 rounded-md border-gray-800 m-2 flex items-center justify-between">
+		<div class="bg-white shadow-lg w-full md:w-1/3 lg:w-1/3 flex-grow xl:w-1/5 p-5 border-l-4 rounded-md border-gray-800 m-2 flex items-center justify-between">
 			<div>
 				<h5 class="uppercase text-xs text-gray-800">EARN WALLET</h5>
 				<h3 class="text-xl font-bold">7918</h3>
@@ -19,7 +19,7 @@
 				<svg aria-hidden="true" class="h-8 w-8 text-gray-800" data-prefix="fal" data-icon="comment-dots" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M128 216c-13.3 0-24 10.7-24 24s10.7 24 24 24 24-10.7 24-24-10.7-24-24-24zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24 24-10.7 24-24-10.7-24-24-24zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24 24-10.7 24-24-10.7-24-24-24zM256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 384c-28.3 0-56.3-4.3-83.2-12.8l-15.2-4.8-13 9.2c-23 16.3-58.5 35.3-102.6 39.6 12-15.1 29.8-40.4 40.8-69.6l7.1-18.7-13.7-14.6C47.3 313.7 32 277.6 32 240c0-97 100.5-176 224-176s224 79 224 176-100.5 176-224 176z"/></svg>
 			</div>
 		</div>
-		<div class="bg-white shadow-lg w-1/4 p-5 border-l-4 rounded-md border-red-500 m-2 flex items-center justify-between">
+		<div class="bg-white shadow-lg w-full md:w-1/3 lg:w-1/3 flex-grow xl:w-1/5 p-5 border-l-4 rounded-md border-red-500 m-2 flex items-center justify-between">
 			<div>
 				<h5 class="uppercase text-xs text-red-500">TOTAL WALLET</h5>
 				<h3 class="text-xl font-bold">27048</h3>
@@ -28,7 +28,7 @@
 				<svg class="h-8 w-8 text-gray-800" aria-hidden="true" data-prefix="fal" data-icon="calendar-week" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 64h-48V8c0-4.4-3.6-8-8-8h-16c-4.4 0-8 3.6-8 8v56H128V8c0-4.4-3.6-8-8-8h-16c-4.4 0-8 3.6-8 8v56H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm16 400c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V192h384v272zm0-304H32v-48c0-8.8 7.2-16 16-16h352c8.8 0 16 7.2 16 16v48zM80 352h288c8.8 0 16-7.2 16-16v-96c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16zm16-96h256v64H96v-64z"/></svg>
 			</div>
 		</div>
-		<div class="bg-white shadow-lg w-1/4 p-5 border-l-4 rounded-md border-green-300 m-2 flex items-center justify-between">
+		<div class="bg-white shadow-lg w-full md:w-1/3 lg:w-1/3 flex-grow xl:w-1/5 p-5 border-l-4 rounded-md border-green-300 m-2 flex items-center justify-between">
 			<div>
 				<h5 class="uppercase text-xs text-green-300">WALLET</h5>
 				<h3 class="text-xl font-bold">19470 BDT</h3>
@@ -38,7 +38,6 @@
 			</div>
 		</div>
 	</div>
-
 	<div class="flex">
 		<div class="bg-white shadow-lg rounded-md w-1/4 m-2">
 			<div class="flex border-b-2 border-gray-100 p-5 items-center justify-between">
@@ -142,7 +141,7 @@
 			    <h3 class="text-md font-bold">Total Business Earning</h3>
 			</div>
 			<div class="bg-white shadow" style="height: 25rem;">
-				
+				<MonthlyIncome/>
 			</div>
 		</div>
 	</div>
@@ -151,9 +150,10 @@
 
 <script>
 import { mapGetters } from 'vuex'
-
+import MonthlyIncome from './../components/MonthlyIncome.vue'
 export default {
 	layout: 'basic',
+	components: { MonthlyIncome },
 
 	metaInfo () {
 		return { title: this.$t('home') }
@@ -165,7 +165,7 @@ export default {
 
 	computed: mapGetters({
 		authenticated: 'auth/check'
-	})
+	}),
 };
 </script>
 
