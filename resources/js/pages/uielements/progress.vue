01eba758f7c924e8c1b88fc5a3ff8b7fb7731638
web-full-stack-developer-nft <jd3606192@gmail.com>
2020-04-18 20:15:57 +0600

    make progress style

--- resources/js/pages/uielements/progress.vue
+++ resources/js/pages/uielements/progress.vue
@@ -1,22 +1,20 @@
 <template>
 <div>
-  <div class="shadow w-full bg-grey-light">
-    <div class="bg-blue text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
+  <div class="shadow w-full bg-grey-500">
+	<div class="bg-blue-500 text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
   </div>
   
-  
-  <div class="shadow w-full bg-grey-light mt-2">
-    <div class="bg-teal text-xs leading-none py-1 text-center text-white" style="width: 55%">55%</div>
+  <div class="shadow w-full bg-grey-500 mt-2">
+	<div class="bg-teal-500 text-xs leading-none py-1 text-center text-white" style="width: 55%">55%</div>
   </div>
-  
-    
-  <div class="shadow w-full bg-grey-light mt-2">
-    <div class="bg-orange text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
+	
+  <div class="shadow w-full bg-grey-500 mt-2">
+	<div class="bg-orange-500 text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
   </div>
-    
-  <div class="shadow w-full bg-grey-light mt-2">
-    <div class="bg-red text-xs leading-none py-1 text-center text-white" style="width: 75%">75%</div>
-  </div>
+	
+	<div class="shadow w-full bg-grey-500 mt-2">
+		<div class="bg-red-500 text-xs leading-none py-1 text-center text-white" style="width: 75%">75%</div>
+	</div>
 </div>
 </template>
 
