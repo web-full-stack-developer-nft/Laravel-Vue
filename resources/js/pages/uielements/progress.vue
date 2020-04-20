05a354761b91e070626cbc4f1726f45f97ea82d2
web-full-stack-developer-nft <jd3606192@gmail.com>
2020-04-20 22:19:21 +0600

    make one more progress

--- resources/js/pages/uielements/progress.vue
+++ resources/js/pages/uielements/progress.vue
@@ -1,49 +1,97 @@
 <template>
-<div class="bg-white">
-  	<div class="shadow w-full bg-green-100">
-		<div class="bg-gray-500 text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
-  	</div>
-  
-  	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-red-500 text-xs leading-none py-1 text-center text-white" style="width: 55%">55%</div>
-  	</div>
-	
-  	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-orange-500 text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
-  	</div>
-	
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-yellow-500 text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
-	</div>
+<div>
+	<div class="w-1/2 mx-auto bg-white p-3 m-3">
+	  	<div class="shadow w-full bg-green-100">
+			<div class="bg-gray-500 text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
+	  	</div>
+	  
+	  	<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-red-500 text-xs leading-none py-1 text-center text-white" style="width: 55%">55%</div>
+	  	</div>
+		
+	  	<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-orange-500 text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
+	  	</div>
+		
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-yellow-500 text-xs leading-none py-1 text-center text-white" style="width: 45%">45%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-green-500 text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-green-500 text-xs leading-none py-1 text-center text-white" style="width: 65%">65%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-green-900 text-xs leading-none py-1 text-center text-white" style="width: 35%">35%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-green-900 text-xs leading-none py-1 text-center text-white" style="width: 35%">35%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-teal-500 text-xs leading-none py-1 text-center text-white" style="width: 95%">95%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-teal-500 text-xs leading-none py-1 text-center text-white" style="width: 95%">95%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-blue-500 text-xs leading-none py-1 text-center text-white" style="width: 15%">15%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-blue-500 text-xs leading-none py-1 text-center text-white" style="width: 15%">15%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-indigo-500 text-xs leading-none py-1 text-center text-white" style="width: 95%">95%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-indigo-500 text-xs leading-none py-1 text-center text-white" style="width: 95%">95%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-purple-500 text-xs leading-none py-1 text-center text-white" style="width: 5%">5%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-purple-500 text-xs leading-none py-1 text-center text-white" style="width: 5%">5%</div>
+		</div>
 
-	<div class="shadow w-full bg-green-100 mt-3">
-		<div class="bg-pink-500 text-xs leading-none py-1 text-center text-white" style="width: 85%">85%</div>
-	</div>
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-pink-500 text-xs leading-none py-1 text-center text-white" style="width: 85%">85%</div>
+		</div>
 
+	</div>
+	<div class="w-1/2 mx-auto bg-white p-3 m-3">
+	  	<div class="shadow w-full bg-green-100">
+			<div class="bg-gray-500 text-xs leading-none text-center text-white" style="width: 45%">45%</div>
+	  	</div>
+	  
+	  	<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-red-500 text-xs leading-none text-center text-white" style="width: 55%">55%</div>
+	  	</div>
+		
+	  	<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-orange-500 text-xs leading-none text-center text-white" style="width: 65%">65%</div>
+	  	</div>
+		
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-yellow-500 text-xs leading-none text-center text-white" style="width: 45%">45%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-green-500 text-xs leading-none text-center text-white" style="width: 65%">65%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-green-900 text-xs leading-none text-center text-white" style="width: 35%">35%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-teal-500 text-xs leading-none text-center text-white" style="width: 95%">95%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-blue-500 text-xs leading-none text-center text-white" style="width: 15%">15%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-indigo-500 text-xs leading-none text-center text-white" style="width: 95%">95%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-purple-500 text-xs leading-none text-center text-white" style="width: 5%">5%</div>
+		</div>
+
+		<div class="shadow w-full bg-green-100 mt-3">
+			<div class="bg-pink-500 text-xs leading-none text-center text-white" style="width: 85%">85%</div>
+		</div>
+
+	</div>
 </div>
 </template>
 
