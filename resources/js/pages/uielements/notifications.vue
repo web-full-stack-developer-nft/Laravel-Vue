ad4714ecfb9c0dac8ac64467da61f78621810c0a
web-full-stack-developer-nft <jd3606192@gmail.com>
2020-04-20 23:22:32 +0600

    make 3 notifications ui

--- resources/js/pages/uielements/notifications.vue
+++ resources/js/pages/uielements/notifications.vue
@@ -37,6 +37,28 @@
 	  </div>
 	</div>
 
+	<div class="flex mt-3">
+	  	<div class="m-auto">
+	    	<div class="bg-white rounded-lg border-gray-300 border p-3 shadow-lg">
+		  		<div class="w-2 bg-gray-800"></div>
+			 	<div class="flex items-center px-2 py-3">
+				    <img class="w-12 h-12 object-cover rounded-full" src="https://images.unsplash.com/photo-1477118476589-bff2c5c4cfbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+				    <div class="mx-3">
+				      <h2 class="text-xl font-semibold text-gray-800">Hello john</h2>
+				      <p class="text-gray-600">Sara was replied on the <a href="#" class="text-blue-500">Upload Image</a>.</p>
+				    </div>
+			  	</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="p-2">
+	    <div class="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
+	      <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-3 justify-center items-center">Pink</span>
+	      <span class="inline-flex px-2">Donec sit amet neque risus. Pellentesque leo mauris, dictum et ligula in.</span>
+	    </div>
+	</div>
+
 	<!-- notificatoin box 2 --> 
 	<div class="shadow-lg rounded-lg bg-white mx-auto m-8 p-4 notification-box flex w-1/5">
 	    <div class="pr-2">
